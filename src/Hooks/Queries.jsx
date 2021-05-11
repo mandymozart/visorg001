32770a0c5e032d95489d047ebada7c8da6f0b70e
tip <tip@foryouandyourcustomers.com>
2021-05-11 09:21:00 +0200

    refactor queries and add tracking

--- src/Hooks/Queries.jsx
+++ src/Hooks/Queries.jsx
@@ -1,54 +1,62 @@
 import { useMutation, useQuery } from "react-query";
 import axios from "axios";
+import config from "../config";
+
+const getUrl = (endpoint, query) => {
+  const url = new URL(config.apiURL + endpoint);
+  if (query) url.search = new URLSearchParams(query);
+  return url;
+};
 
 export const useGetUsers = () => {
   return useQuery("getUsers", () =>
-    fetch("https://visinn001.viennastruggle.at/api/users").then((res) =>
-      res.json()
-    )
+    fetch(getUrl("/users")).then((res) => res.json())
   );
 };
 
-export const useGetUser = ({ id }) => {
-  return useQuery(`getUser-${id}`, () =>
-    fetch(
-      "https://visinn001.viennastruggle.at/api/users?id=" + id
-    ).then((res) => res.json())
+export const useGetUser = (query) => {
+  const url = getUrl("/users", query);
+  return useQuery(`getUser-${query.id}`, () =>
+    fetch(url).then((res) => res.json())
   );
 };
 
 export const useGetProjects = (query) => {
-  console.log(query);
-  const url = new URL(`https://visinn001.viennastruggle.at/api/projects`);
-  url.search = new URLSearchParams(query);
+  const url = getUrl("/projects", query);
   return useQuery(`getProjects-${query.status}${query.ownerId}`, () =>
     fetch(url).then((res) => res.json())
   );
 };
 
 export const useGetProject = (id) => {
+  const url = getUrl("/projects", { id });
   return useQuery(`getProject-${id}`, () =>
-    fetch(
-      `https://visinn001.viennastruggle.at/api/projects?id=${id}`
-    ).then((res) => res.json())
+    fetch(url).then((res) => res.json())
   );
 };
 
 export const useAddProject = () => {
+  const url = getUrl("/project-add");
   return useMutation((data) =>
     axios
-      .post(
-        `https://visinn001.viennastruggle.at/api/project-add`,
-        data,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((res) => { 
-        console.log(res)
-        return res.data
+      .post(url, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
+      .then((res) => res.data)
   );
+};
+
+export const useAddTracking = () => {
+  const url = getUrl("/tracking-add");
+  return useMutation((data) => {
+    axios
+      .post(url, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => res.data);
+  });
 };

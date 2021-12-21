00fa2fa4404fb6702b8e2b70fd5a1256417a71ee
Mandy Mozart <mandymozart@viennastruggle.com>
2021-12-21 18:04:22 +0100

    Add new fields to project

--- src/Pages/Projects/Briefing.jsx
+++ src/Pages/Projects/Briefing.jsx
@@ -18,8 +18,12 @@ const Briefing = () => {
     <Container>
       <Header>
         <h3>{project.title}</h3>
-        {user?.sub === project.ownerId && <Role>Owner</Role>}
+        {project.imageUrl ?? <img src={project.imageUrl} alt={project.imageUrl} />}
+        {user?.sub === project.ownerId && <Role>Owner</Role>} 
+        {project.workspaceUrl ?? <a class="button" href={project.workspaceUrl} target="_blank" rel="noopener noreferrer">Workspace</a>} 
+        
         <Tags tags={project.tags} />
+        {project.videoUrl ?? <a class="button" href={project.videoUrl} target="_blank" rel="noopener noreferrer">Play Trailer</a>} 
       </Header>
       <Description source={project.description} />
     </Container>

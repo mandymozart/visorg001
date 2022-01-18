53970020775f812d0032dcd794a8ea716c280cce
Mandy Mozart <mandymozart@viennastruggle.com>
2022-01-18 13:40:08 +0100

    moving struggles

--- src/Pages/Projects/ProjectsSection.jsx
+++ src/Pages/Projects/ProjectsSection.jsx
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled";
 import React from "react";
+import TextLoop from "react-text-loop";
 import { SimpleProjectList } from "../../Components/SimpleProjectList";
 
 const Container = styled.section`
   color: var(--background);
-  text-align: center;
+  text-align: left;
+  padding: 1rem;
   img {
     max-width: 100%;
     &.full {
@@ -12,24 +14,29 @@ const Container = styled.section`
     }
   }
 `;
-const ProjectsSection = ({projects}) => {
-  console.log(projects)
+const ProjectsSection = ({ projects }) => {
+  console.log(projects);
   return (
     <Container>
       <div className="page__header">
         <h1>Releases</h1>
         <p>
-          We share our stories of through music.<br />
+          We share our stories of through music.
+          <br />
           Our stories talk of <br />
-          emotional struggles, <br />
-          physical struggles, <br />
-          ancestral struggles,<br />
-          mental struggles,<br />
-          identity struggles,<br />
-          orientational struggles &amp;<br />
-          political struggles<br />
+          <TextLoop>
+            <span>emotional </span>
+            <span>physical </span>
+            <span>ancestral</span>
+            <span>mental</span>
+            <span>identity</span>
+            <span>orientational</span>
+            <span>political</span>
+          </TextLoop>{" "}
+          &nbsp;struggles, <br />
           &mdash; <br />
-          Struggles to adopt to a new age.<br />
+          Struggles to adopt to a new age.
+          <br />
         </p>
       </div>
 
